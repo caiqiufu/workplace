be71663c0db0f467d82fa560ff3844e63acc3152
caiqiufu <caiqiufu@gmail.com>
2016-01-26 20:50:48 -0800

    tt

---
+++ unieap/src/com/unieap/demo/TestVO.java
@@ -0,0 +1,16 @@
+package com.unieap.demo;
+
+import com.unieap.base.vo.BaseVO;
+
+public class TestVO extends BaseVO{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
