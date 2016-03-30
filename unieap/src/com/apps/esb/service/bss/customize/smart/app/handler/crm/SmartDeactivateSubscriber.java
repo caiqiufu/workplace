62034992da7eec6bdf76e47697605f1c385c6c6e
caiqiufu@gmail.com <caiqiufu@gmail.com>
2016-03-30 23:47:59 +0800

    tt

---
+++ unieap/src/com/apps/esb/service/bss/customize/smart/app/handler/crm/SmartDeactivateSubscriber.java
@@ -0,0 +1,88 @@
+
+package com.apps.esb.service.bss.customize.smart.app.handler.crm;
+
+import java.util.Map;
+
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPMessage;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import com.apps.esb.service.bss.BssServiceUtils;
+import com.apps.esb.service.bss.customize.smart.app.handler.CustSoapMessageHandler;
+import com.apps.esb.service.bss.element.RequestInfo;
+import com.apps.esb.service.bss.element.ResponsetInfo;
+import com.apps.esb.service.bss.handler.BizHandler;
+import com.apps.esb.service.bss.handler.ProcessResult;
+import com.apps.esb.service.bss.interfacecall.SoapCallUtils;
+
+@Service("deActivateSubscriber_1")
+public class SmartDeactivateSubscriber extends CustSoapMessageHandler implements BizHandler {
+
+	@Override
+	public ProcessResult process(RequestInfo requestInfo, Map<String, String> handler,
+			Map<String, Object> extParameters) throws Exception {
+		RequestInfo newRequestInfo = BssServiceUtils.copyRequestInfo(requestInfo);
+		newRequestInfo.getRequestHeader().setBizCode("deActivateSubscriber");
+		return SoapCallUtils.process(this, newRequestInfo, requestInfo.getRequestBody().getServiceNumber(),
+				requestInfo.getRequestBody().getExtParameters(), handler, "ws.crm.query.timeout");
+	}
+
+	@Override
+	public RequestInfo getRequestInfoFromSOAPMessage(SOAPMessage request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponsetInfo getResponseInfoFromSOAPMessage(SOAPMessage response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SOAPMessage getRequestSOAPMessage(String serviceNumber, RequestInfo requestInfo) throws Exception {
+		JSONObject json = new JSONObject(requestInfo.getRequestBody().getExtParameters());
+		if (!json.has("status")) {
+			throw new Exception("status is null");
+		}
+		String status = json.getString("status");
+		SOAPMessage message = SoapCallUtils.getMessageFactory().createMessage();
+		this.getCRMSerHeader("SubmitOrderRequest", message);
+		SOAPBodyElement bodyElement = (SOAPBodyElement) message.getSOAPBody().getChildElements().next();
+		SOAPElement requestBodyElement = bodyElement.addChildElement("SubmitRequestBody", "ser");
+		SOAPElement orderElement = requestBodyElement.addChildElement("Order", "ser");
+		orderElement.addChildElement("OrderType", "bas").addTextNode("CO022");
+		SOAPElement orderItemsElement = requestBodyElement.addChildElement("OrderItems", "ser");
+		SOAPElement orderItemElement = orderItemsElement.addChildElement("OrderItem", "bas");
+
+		SOAPElement orderItemInfoElement = orderItemElement.addChildElement("OrderItemInfo", "bas");
+		SOAPElement orderItemTypeElement = orderItemInfoElement.addChildElement("OrderItemType", "bas");
+		orderItemTypeElement.addTextNode("CO022");
+
+		SOAPElement subscriberElement = orderItemElement.addChildElement("Subscriber", "bas");
+		subscriberElement.addChildElement("ServiceNumber", "bas").addTextNode(serviceNumber);
+		subscriberElement.addChildElement("Status", "bas").addTextNode(status);
+		return message;
+	}
+
+	@Override
+	public ProcessResult getResposeResult(SOAPMessage response) throws Exception {
+		ProcessResult result = new ProcessResult();
+		org.w3c.dom.Document document = response.getSOAPPart().getEnvelope().getOwnerDocument();
+		if (document.getElementsByTagName("bas:RetCode").getLength() > 0) {
+
+			String retCode = document.getElementsByTagName("bas:RetCode").item(0).getTextContent();
+			result.setResultCode(retCode);
+		}
+		if (document.getElementsByTagName("bas:RetMsg").getLength() > 0) {
+
+			String retMsg = document.getElementsByTagName("bas:RetMsg").item(0).getTextContent();
+			result.setResultDesc(retMsg);
+		}
+		return result;
+	}
+
+}
